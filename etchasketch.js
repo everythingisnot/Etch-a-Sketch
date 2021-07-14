36bca90577979f6e4fb346e8429c7a2ad4be9dd1
Nerissa <nerissa.wadey@gmail.com>
2021-07-14 11:46:29 -0400

    two boxes in the grid so far

---
+++ etchasketch.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('#container');
+const div1 = document.createElement('div');
+div1.classList.add('divs');
+container.appendChild(div1);
+
+const div2 = document.createElement('div');
+div2.classList.add('divs'); 
+container.appendChild(div2);
+
+
