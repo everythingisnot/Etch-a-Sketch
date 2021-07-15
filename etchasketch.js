b60235118ea13db43ed65fdb2a6ef68c82edc054
Nerissa <nerissa.wadey@gmail.com>
2021-07-15 18:38:07 -0400

    changed back to in line block

--- etchasketch.js
+++ etchasketch.js
@@ -1,10 +1,13 @@
-const container = document.querySelector('#container');
-const div1 = document.createElement('div');
-div1.classList.add('divs');
-container.appendChild(div1);
 
-const div2 = document.createElement('div');
-div2.classList.add('divs'); 
-container.appendChild(div2);
+const containerBox = document.querySelector("#container");
+const div1 = document.createElement("div");
+const div2 = document.createElement("div");
+const div3 = document.createElement("div");
+div1.classList.add("boxes");
+div2.classList.add("boxes");
+div3.classList.add("boxes");
+containerBox.appendChild(div2);
+containerBox.appendChild(div1);
+containerBox.appendChild(div3);
 
 
